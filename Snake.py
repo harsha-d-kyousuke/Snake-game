744121eb8f719b7f7006479380132180a53b1ea3
harsha-d-kyousuke <139533327+harsha-d-kyousuke@users.noreply.github.com>
2024-05-25 17:36:13 +0530

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -1,10 +1,11 @@
-starting_position = [(0, 0), (-20, 0), (-40,0)]
-from turtle import Turtle, Screen
+from turtle import Turtle
 
-Up = 90
-Down = 270
-Right = 0
-Left = 180
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     def __init__(self):
@@ -13,40 +14,48 @@ class Snake:
         self.head = self.segments[0]
 
     def create_snake(self):
-        for position in starting_position:
+        for position in STARTING_POSITIONS:
             self.add_segment(position)
-            
 
     def add_segment(self, position):
         new_segment = Turtle("square")
-        new_segment.color("white")
+        new_segment.color("green")
         new_segment.penup()
         new_segment.goto(position)
         self.segments.append(new_segment)
-        
 
     def extend(self):
-        self.add_segment(self.segments[-1].position())
-
+        # Add a new segment to the snake
+        last_segment = self.segments[-1]
+        self.add_segment(last_segment.position())
 
     def move(self):
+        # Move the snake segments
         for seg_num in range(len(self.segments) - 1, 0, -1):
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.head.forward(20)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        if self.head.heading() != Down:
-            self.head.setheading(Up)
-    def down(self):
-        if self.head.heading() != Up:
-            self.head.setheading(Down)
-    def right(self):
-        if self.head.heading() != Left:
-            self.head.setheading(Right)
-    def left(self):
-        if self.head.heading() != Right:
-            self.head.setheading(Left)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
-    
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
+
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def reset(self):
+        # Reset snake position and body
+        for segment in self.segments:
+            segment.goto(1000, 1000)  # Move segments off-screen
+        self.segments.clear()
+        self.create_snake()
