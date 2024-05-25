9f14cd74307e2bdabdb1411b4f753e307d3feff3
harsha-d-kyousuke <139533327+harsha-d-kyousuke@users.noreply.github.com>
2024-05-25 17:35:01 +0530

    Update food.py
    custom

--- food.py
+++ food.py
@@ -3,18 +3,22 @@ import random
 
 class Food(Turtle):
     def __init__(self):
-      super().__init__()
-      self.shape("circle")
-      self.penup()
-      self.shapesize(stretch_len=0.5, stretch_wid=0.5)
-      self.color("red")
-      self.speed("fastest")
-      self.refresh()
-      
+        super().__init__()
+        self.shape("circle")
+        self.penup()
+        self.shapesize(stretch_len=0.5, stretch_wid=0.5)
+        self.color("red")
+        self.speed("fastest")
+        self.refresh()
+
     def refresh(self):
-      random_x = random.randint(-280, 280)
-      random_y = random.randint(-280, 280)
-      self.goto(random_x, random_y)
+        random_x = random.randint(-280, 280)
+        random_y = random.randint(-280, 280)
+        self.goto(random_x, random_y)
+
+    def reset(self):
+        # Reset food position to a new random location
+        self.refresh()
 
        
 
